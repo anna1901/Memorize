5b56c885e8b0fb50e331a59235ee123532990b3a
anna1901 <anna.olak1901@gmail.com>
2025-01-02 16:56:45 +0100

    Add more cards and buttons to change card count

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,29 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻", "🎃", "😈", "💀", "👽", "🧟", "🥷🏻", "🕸️", "🕷️", "🦇", "🔪", "🪦", "🔮"]
+    
+    @State var cardCount = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardAdjusters
+        }
+        .padding()
+    }
+    
+    private var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundStyle(.orange)
-        .padding()
+    }
+    
+    private var cardAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .font(.largeTitle)
+    }
+    
+    private var cardRemover: some View {
+        Button("-") {
+            if cardCount > 0 {
+                cardCount -= 1
+            }
+        }
+    }
+    
+    private var cardAdder: some View {
+        Button("+") {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }
     }
 }
 
 struct CardView: View {
-    @State var isFaceUp = false
+    let content: String
+    @State var isFaceUp = true
     
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
                 base.fill(Color.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
-            } else {
+                Text(content).font(.largeTitle)
+            if !isFaceUp {
                 base.fill()
             }
         }
